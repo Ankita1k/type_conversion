f39a8612ecc8a5e90e50b89d3535eecd81620acf
Ankita Kumari <chaubeyankita860@gmail.com>
2024-05-11 01:26:31 +0530

    Added type_conversion

---
+++ type_conversion/tute1.js
@@ -0,0 +1,18 @@
+// let a = true;
+
+// console.log(typeof a)
+// console.log("value of a",a)
+// let b = console.log(typeof Number(a))
+// console.log(typeof b)
+// console.log("value of b",b)
+
+// "33" -> 33
+// "ab44" -> undefined
+// true -> true  false -> false
+
+let name = 1
+let ne = Boolean(name)
+console.log(ne)
+
+// 1 => true  0 => false
+// "" => false  "abc" => true
